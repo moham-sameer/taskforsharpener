ce3eec32bd4d09ef37c8b63af402605f8524998a
Mohammad Sameer <ms4492222@gmail.com>
2024-10-09 14:15:00 +0530

    writing on different routes

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,7 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    res.end("Hello, World!")
+    if(req.url=='/home'){
+        res.write("Welcome Home!")
+    }else if(req.url == '/about'){
+        res.write("Welcome to about Us page!")
+    }else if(req.url == '/node'){
+        res.write("Welcome to my Node js project!")
+    }
+    res.end()
 })
 
 server.listen(4000,console.log("server is listening on port 4000..."))
